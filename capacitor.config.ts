39259673924e63196fa00f64a8e6babfb3ed49c9
Janaina Tavares <52297249+jana-dev@users.noreply.github.com>
2024-10-10 14:29:49 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meuAppFormulario',
+  webDir: 'www'
+};
+
+export default config;
